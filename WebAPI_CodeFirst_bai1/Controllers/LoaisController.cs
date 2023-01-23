585fb2354328c9261ed3c167dbbba5c3addcf82c
DoanQuangHuy <doanhuyxh@outlook.com.vn>
2023-01-23 10:53:23 +0700

    update

---
+++ WebAPI_CodeFirst_bai1/Controllers/LoaisController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_CodeFirst_bai1.Models;
+using WebAPI_CodeFirst_bai1.Serveices;
+
+namespace WebAPI_CodeFirst_bai1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly ILoaiRepository _loaiRepository;
+
+        public LoaisController(ILoaiRepository loaiRepository)
+        {
+            _loaiRepository = loaiRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_loaiRepository.GetAll());
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetLoaiById(int id)
+        {
+            try
+            {
+                return Ok(_loaiRepository.GetById(id));
+            }
+            catch
+            {
+                return StatusCode(404);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Loai loai)
+        {
+            try
+            {
+                _loaiRepository.Update(id, loai);
+                return NoContent();
+            }
+            catch { return StatusCode(500); }
+        }
+
+        [HttpPost]
+        public IActionResult Create(LoaiViewModel loaivm)
+        {
+            try
+            {
+                return Ok(_loaiRepository.Add(loaivm));
+            }
+            catch { return StatusCode(500); }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _loaiRepository.Delete(id);
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+
+        }
+    }
+}
---
+++ WebAPI_CodeFirst_bai1/Serveices/LoaiResository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI_CodeFirst_bai1.Data;
+using WebAPI_CodeFirst_bai1.Models;
+
+namespace WebAPI_CodeFirst_bai1.Serveices
+{
+    public class LoaiResository : ILoaiRepository
+    {
+        private readonly HangHoaDBContext _context;
+
+        public LoaiResository (HangHoaDBContext context)
+        {
+            _context = context;
+        }
+
+        public Loai Add(LoaiViewModel loai)
+        {
+            try
+            {
+                var _loai = new Loai { TenLoai = loai.TenLoai };
+                _context.Add(_loai);
+                _context.SaveChanges();
+                return _loai;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private Loai BadRequest(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            var loai = _context.Loai.ToList().SingleOrDefault(l => l.Id == id);
+            _context.Remove(loai);
+        }
+
+        public List<Loai> GetAll()
+        {
+            var ds = _context.Loai.ToList();
+            return ds;
+        }
+
+        public Loai GetById(int id)
+        {
+            var loai = _context.Loai.ToList().SingleOrDefault(l => l.Id == id);
+            return loai;
+        }       
+
+        public void Update(int id, Loai loai)
+        {
+            var _loai = _context.Loai.ToList().SingleOrDefault(l => l.Id == id);
+            _loai.TenLoai= loai.TenLoai;
+           _context.SaveChanges();
+        }
+    }
+}
